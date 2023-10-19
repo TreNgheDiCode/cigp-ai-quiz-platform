4235819369748b720b8ea5cec8a29ab72fee3366
TreNgheDiCode <gabayan170@gmail.com>
2023-10-19 16:54:03 +0700

    Create Game Endpoint

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { ThemeProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import Script from "next/script";
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
   return (
--- lib/next-auth.ts
+++ lib/next-auth.ts
@@ -1,14 +1,26 @@
-import { NextAuthOptions, DefaultSession, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "./db";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/db";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
+      // ...other properties
+      // role: UserRole;
     } & DefaultSession["user"];
   }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
 }
 
 declare module "next-auth/jwt" {
@@ -21,18 +33,17 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     jwt: async ({ token }) => {
       const db_user = await prisma.user.findFirst({
         where: {
-          email: token.email,
+          email: token?.email,
         },
       });
-
       if (db_user) {
         token.id = db_user.id;
       }
-
       return token;
     },
     session: ({ session, token }) => {
@@ -42,11 +53,9 @@ export const authOptions: NextAuthOptions = {
         session.user.email = token.email;
         session.user.image = token.picture;
       }
-
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
