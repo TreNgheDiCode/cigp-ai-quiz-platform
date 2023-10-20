e21ff658080931727302b1cd4ed0d55c300e4fd2
TreNgheDiCode <gabayan170@gmail.com>
2023-10-20 21:03:40 +0700

    MCQ Page UI

---
+++ app/play/mcq/[gameId]/page.tsx
@@ -0,0 +1,41 @@
+import MCQ from "@/components/MCQ";
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/next-auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    gameId: string;
+  };
+};
+
+const McqPage = async ({ params: { gameId } }: Props) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return redirect("/");
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
+        },
+      },
+    },
+  });
+
+  if (!game || game.gameType !== "mcq") {
+    return redirect("/quiz");
+  }
+  return <MCQ game={game} />;
+};
+
+export default McqPage;
